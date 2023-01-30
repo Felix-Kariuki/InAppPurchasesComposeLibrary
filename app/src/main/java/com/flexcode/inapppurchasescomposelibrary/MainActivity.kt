b58c5e439ce9bd540db13efdf9d0f9f60aa1cacf
Felix-Kariuki <felixmurimi280@gmail.com>
2023-01-30 23:20:20 +0300

    implementation

--- app/src/main/java/com/flexcode/inapppurchasescomposelibrary/MainActivity.kt
+++ app/src/main/java/com/flexcode/inapppurchasescomposelibrary/MainActivity.kt
@@ -1,16 +1,38 @@
+/*
+ * Copyright 2022 Felix Kariuki.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.flexcode.inapppurchasescomposelibrary
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.flexcode.inapppurchasescompose.InAppPurchasesHelper
 import com.flexcode.inapppurchasescomposelibrary.ui.theme.InAppPurchasesComposeLibraryTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,25 +45,48 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
-                    Text(text = "Hello",
-                    modifier = Modifier.clickable {
-                    })
+                    val billingPurchaseHelper = InAppPurchasesHelper(this,"test_product")
+                    billingPurchaseHelper.setUpBillingPurchases()
+
+                    val purchaseDone by billingPurchaseHelper.purchaseDone.collectAsState(false)
+                    val productName by billingPurchaseHelper.productName.collectAsState("")
+                    val purchaseStatus by billingPurchaseHelper.purchaseStatus.collectAsState("")
+
+                    Button(
+                        onClick = {
+                            billingPurchaseHelper.initializePurchase()
+                        },
+
+                        shape = RoundedCornerShape(32.dp),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = MaterialTheme.colorScheme.surface,
+                            contentColor = MaterialTheme.colorScheme.onSurface
+                        ),
+                        modifier = Modifier.wrapContentSize(),
+                        enabled = purchaseDone
+                    ) {
+                        Icon(
+                            Icons.Filled.Add,
+                            contentDescription = "InApp Purchases",
+                            modifier = Modifier.size(ButtonDefaults.IconSize)
+                        )
+                        Spacer(Modifier.size(ButtonDefaults.IconSpacing))
+                        Text(
+                            "In App Purchases",
+                            fontSize = 16.sp,
+                            modifier = Modifier.padding(8.dp)
+                        )
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     InAppPurchasesComposeLibraryTheme {
-        Greeting("Android")
+
     }
 }
